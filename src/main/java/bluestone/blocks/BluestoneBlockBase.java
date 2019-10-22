773730a483fbdd1dc65f5727651906dc6366e071
CallxMeFelix <felix.martinez7@upr.edu>
2019-10-22 13:38:18 -0400

    Block now drop item when mined

--- src/main/java/bluestone/blocks/BluestoneBlockBase.java
+++ src/main/java/bluestone/blocks/BluestoneBlockBase.java
@@ -9,24 +9,15 @@ import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import scala.util.Random;
 
 public class BluestoneBlockBase extends Block implements IHasModel {
 	public BluestoneBlockBase(String name, Material material) {
 		super(material);
 		setUnlocalizedName(name);
 		setRegistryName(name);
-<<<<<<< HEAD
-		setCreativeTab(CreativeTabs.REDSTONE);
 
-=======
 		setCreativeTab(Main.tabBluestone);
-		
->>>>>>> branch 'master' of https://github.com/uprm-ciic4010-f19/minecraft-project-redstone-engineers.git
+
 		BluestoneBlocks.BLOCKS.add(this);
 		ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
 	}
@@ -36,15 +27,5 @@ public class BluestoneBlockBase extends Block implements IHasModel {
 		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "normal");
 
 	}
-	@SideOnly(Side.CLIENT)
-	public void randomDisplayTick(World world, int x, int y, int z, Random random) {
-		float f1 = (float) x+ 0.5F;
-		float f2 = (float) y+ 1.1F;
-		float f3 = (float) z+ 0.5F;
-		float f4 = random.nextFloat() * 0.6F - 0.3F;
-		float f5 = random.nextFloat() * -0.6F - - 0.3F;
-		
-		world.spawnParticle(EnumParticleTypes.FLAME, (double)(f1+f4), (double)f2 , (double)(f3+f5), 1.0D, 1.0D, 1.0D,1);
-		
-	}
+
 }
--- src/main/java/bluestone/init/BluestoneBlocks.java
+++ src/main/java/bluestone/init/BluestoneBlocks.java
@@ -3,13 +3,15 @@ package bluestone.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import bluestone.Main;
 import bluestone.blocks.BluestoneBlock;
-import bluestone.blocks.BluestoneBlockBase;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
+
 public class BluestoneBlocks {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	public static final Block BLUESTONE_BLOCK = new BluestoneBlock("bluestone_block", Material.REDSTONE_LIGHT);
-	 
+	public static final Block BLUESTONE_BLOCK = new BluestoneBlock("bluestone_block", Material.ROCK, ModItems.BLUESTONE_DUST, 1, 5).setHardness(3f).setResistance(5f).setCreativeTab(Main.tabBluestone);
+
+
 }
