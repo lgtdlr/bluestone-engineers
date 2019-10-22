05a0fa079e90c74592feb36d11b4fff31b298cb3
CallxMeFelix <felix.martinez7@upr.edu>
2019-10-22 11:23:55 -0400

    Changing how new blocks are implemented

--- src/main/java/bluestone/init/BluestoneBlocks.java
+++ src/main/java/bluestone/init/BluestoneBlocks.java
@@ -3,12 +3,13 @@ package bluestone.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import bluestone.blocks.BluestoneBlock;
 import bluestone.blocks.BluestoneBlockBase;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class BluestoneBlocks {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	public static final Block BLUESTONE_BLOCK = new BluestoneBlockBase("bluestone_block", Material.IRON);
+	public static final Block BLUESTONE_BLOCK = new BluestoneBlock("bluestone_block", Material.REDSTONE_LIGHT);
 	 
 }
