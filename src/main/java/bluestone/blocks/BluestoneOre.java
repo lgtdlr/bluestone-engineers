4d3b1eeeea73a3f82753989fdf96b4085f78b986
Leonardo Torres <leonardo.torres5@upr.edu>
2019-10-23 17:11:34 -0400

    Added bluestone block

---
+++ src/main/java/bluestone/blocks/BluestoneOre.java
@@ -0,0 +1,83 @@
+package bluestone.blocks;
+
+import java.util.Random;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+
+public class BluestoneOre extends BluestoneBlockBase {
+	Item itemDrop;
+	int minDrop=1;
+	int maxDrop=1;
+	
+	public BluestoneOre(String name, Material material) {
+		super(name, material);
+		setSoundType(SoundType.STONE);
+		setHardness(5.5F);
+		setResistance(15.0F);
+		setHarvestLevel("pickaxe", 2);
+		setLightLevel(1.0F);
+
+	}
+
+	public BluestoneOre(String name, Material material, Item toDrop) {
+		this(name, material, toDrop, 1, 1);
+		setSoundType(SoundType.STONE);
+		setHardness(5.5F);
+		setResistance(15.0F);
+		setHarvestLevel("pickaxe", 2);
+		setLightLevel(1.0F);
+	}
+
+	public BluestoneOre(String name, Material material, Item toDrop, int dropAmount) {
+		this(name, material, toDrop, dropAmount, dropAmount);
+		setSoundType(SoundType.STONE);
+		setHardness(5.5F);
+		setResistance(15.0F);
+		setHarvestLevel("pickaxe", 2);
+		setLightLevel(1.0F);
+	}
+
+	public BluestoneOre(String name, Material material, Item itemDrop,int minDrop,int maxDrop) {
+		super(name, material);
+		this.itemDrop = itemDrop;
+		this.minDrop = minDrop;
+		this.maxDrop= maxDrop;
+		setSoundType(SoundType.STONE);
+		setHardness(5.5F);
+		setResistance(15.0F);
+		setHarvestLevel("pickaxe", 2);
+		setLightLevel(1.0F);
+		
+
+	}
+	@Override
+	public Item getItemDropped(IBlockState state, Random random, int i)  {
+		return itemDrop == null? Item.getItemFromBlock(this):itemDrop;
+	}
+
+	@Override
+	public int quantityDropped(Random random) {
+		return this.minDrop + random.nextInt(this.maxDrop-this.minDrop+1);
+	}
+	@Override
+	public int quantityDroppedWithBonus(int fortune, Random random)
+	{
+		if (fortune > 0 && Item.getItemFromBlock(this) != this.getItemDropped(this.getDefaultState(), random, fortune))
+		{
+			int i = random.nextInt(fortune + 2) - 1;
+			if (i < 0)
+			{
+				i = 0;
+			}
+			return this.quantityDropped(random) * (i + 1);
+		}
+		else
+		{
+			return this.quantityDropped(random);
+		}
+	}
+}
+
--- src/main/java/bluestone/init/BluestoneBlocks.java
+++ src/main/java/bluestone/init/BluestoneBlocks.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 import bluestone.Main;
 import bluestone.blocks.BluestoneBlock;
+import bluestone.blocks.BluestoneOre;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 
 public class BluestoneBlocks {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	public static final Block BLUESTONE_BLOCK = new BluestoneBlock("bluestone_block", Material.ROCK, ModItems.BLUESTONE_DUST, 1, 5).setHardness(3f).setResistance(5f).setCreativeTab(Main.tabBluestone).setLightLevel(1.0F);
-	
-	
+	public static final Block BLUESTONE_ORE = new BluestoneOre("bluestone_ore", Material.ROCK, ModItems.BLUESTONE_DUST, 1, 5);
+	public static final Block BLUESTONE_BLOCK = new BluestoneBlock("bluestone_block", Material.ROCK);
 }
