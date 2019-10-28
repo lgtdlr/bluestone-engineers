2afd4ddabfbd29d48051c8751c6a4d2744243c79
Leonardo Torres De la Rosa <leonardo.torres5@upr.edu>
2019-10-28 14:08:30 -0400

    Added laser gun with placeholder texture

--- src/main/java/bluestone/init/ModItems.java
+++ src/main/java/bluestone/init/ModItems.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import bluestone.items.ItemBase;
 import bluestone.items.usable.ToolAxe;
+import bluestone.items.usable.ToolBow;
 import bluestone.items.usable.ToolHoe;
 import bluestone.items.usable.ToolPickaxe;
 import bluestone.items.usable.ToolSpade;
@@ -17,6 +18,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemSpade;
@@ -43,6 +45,7 @@ public class ModItems {
 	public static final ItemSpade BLUESTONE_SHOVEL = new ToolSpade("bluestone_shovel", TOOL_MATERIAL_BLUESTONE);
 	public static final ItemAxe BLUESTONE_AXE = new ToolAxe("bluestone_axe", TOOL_MATERIAL_BLUESTONE);
 	public static final ItemHoe BLUESTONE_HOE = new ToolHoe("bluestone_hoe", TOOL_MATERIAL_BLUESTONE);
+	public static final ItemBow BLUESTONE_BOW = new ToolBow("bluestone_bow");
 	
 	//Custom Items
 	public static final Item BLUESTONE_DUST = new ItemBase("bluestone_dust");
